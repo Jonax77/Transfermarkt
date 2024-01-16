7633c2677cb59922c083f4da5de38239d40b97e1
yun <yun@8glabs.com>
2024-01-16 14:11:33 -0800

    init

---
+++ Transfermarkt/Transfermarkt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transfermarkt
+//
+//  Created by Zhiyun Lin on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transfermarkt/Transfermarkt/TransfermarktApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransfermarktApp.swift
+//  Transfermarkt
+//
+//  Created by Zhiyun Lin on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TransfermarktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
